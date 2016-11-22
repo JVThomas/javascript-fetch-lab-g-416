7ab2aedca9ff1ca9c8fc34005316ef2f6ceda776
Justin Thomas <jthomas0209@gmail.com>
2016-11-22 12:40:29 -0500

    last two tests are not written well

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
+const gitHubBase =  'https://api.github.com/';
+const repo =  'JVThomas/javascript-fetch-lab'
+
 function getIssues() {
+	fetch(`${gitHubBase}repos/${repo}/issues`).then(res => res.json()).then(json => showIssues(json));
 }
 
 function showIssues(json) {
+	const template = Handlebars.compile(document.getElementById('issues-template').innerHTML);
+	document.getElementById('issues').innerHTML = template(json);
 }
 
 function createIssue() {
+	fetch(`${gitHubBase}repos${repo}/issues`,{
+		method:`post`,
+		headers:{
+			Authorization: `token ${getToken()}`
+		},
+		title: 'New issue',
+		body:	'test body'
+	}).then(res=> rs.json()).then(json => showIssues(json));
 }
 
 function showResults(json) {
+	const template = Handlebars.compile(document.getElementById('repo-template').innerHTML);
+	document.getElementById('results').innerHTML = template(json);
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+	const token = getToken();
+	const fork = 'learn-co-curriculum/javascript-fetch-lab'
+	fetch(`${gitHubBase}repos/${fork}/forks`, {
+		headers:{
+			Authorization: 'token ${token}}'
+		},
+		method: 'post'
+	}).then(res => res.json()).then(json => showResults(json));
+	//use fetch to fork it!
 }
 
 function getToken() {
